9685a04f1bd7ed6a5c8e43d3bea213cf6e287485
Gloria-46 <sngloria47@gmail.com>
2025-04-03 17:09:33 +0300

    Delete webdevelopment directory

--- webdevelopment/lightbox.js
+++
@@ -1,8 +0,0 @@
-function openLightbox(src) {
-    document.getElementById("lightbox").style.display = "flex";
-    document.getElementById("lightbox-img").src = src;
-}
-
-function closeLightbox() {
-    document.getElementById("lightbox").style.display = "none";
-}
